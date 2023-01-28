799d8cff404d43934cc48c5a8991c99284d986ba
rexzheng <admin@example.com>
2023-01-28 17:30:05 +0800

    “flask13”

--- flaskzheng.py
+++ flaskzheng.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/v1')
 def hello_word():
-    return "Hello World I am Rex zheng11111-111111"
+    return "Hello World I am Rex zheng"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
